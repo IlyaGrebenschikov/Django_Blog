fc0985e285f9dfad7ca3bbc8cb6e3231a092e351
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-09 16:47:43 +0400

    base dir path

--- src/config/settings.py
+++ src/config/settings.py
@@ -22,7 +22,7 @@ from .db_settings import (
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
