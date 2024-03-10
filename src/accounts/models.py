7146a6ab7fd2622c630e76efe5c468493361de87
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-11 00:40:04 +0400

    new field for user and migrations

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 
@@ -5,3 +7,4 @@ class User(models.Model):
     username = models.CharField(max_length=30, null=False)
     email = models.EmailField(unique=True, null=False)
     password = models.CharField(null=False)
+    was_created = models.DateTimeField(default=datetime.now())
