eced63ddffed85f93d257e7797310c741a53e1ed
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-11 01:46:39 +0400

    user models & migrations & admin

--- src/config/settings.py
+++ src/config/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
     'main.apps.MainConfig',
     'accounts.apps.AccountsConfig'
 ]
