222b0f3dd0653119064f9d9c1a0910f897bded71
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-03-12 22:33:37 +0400

    login view and template

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
 from django.contrib.auth.forms import UserCreationForm
@@ -27,3 +29,16 @@ class UserDetailView(DetailView):
     model = MyUser
     template_name = 'accounts/details_view.html'
     context_object_name = 'user'
+
+
+class MyLoginView(LoginView):
+    model = MyUser
+    template_name = 'accounts/login.html'
+    # redirect_authenticated_user = True
+    #
+    # def get_success_url(self):
+    #     return reverse_lazy('login')
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password')
+        return self.render_to_response(self.get_context_data(form=form))
